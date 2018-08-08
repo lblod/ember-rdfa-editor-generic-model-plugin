268f6b536fa7d310f0cea93045d9a03a08986f7f
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-08-08 15:55:04 +0200

    ran plugin generator

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-  name: '@lblod/ember-rdfa-editor-generic-model-plugin'
+  name: '@lblod/ember-rdfa-editor-generic-model-plugin',
+  isDevelopingAddon() {return true;}
 };
