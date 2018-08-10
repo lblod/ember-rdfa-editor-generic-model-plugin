413b9c6164cf89779be7d236d97246b79ac5ef20
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-08-10 19:16:41 +0200

    added component to fetch resource relations

--- addon/services/rdfa-editor-generic-model-plugin.js
+++ addon/services/rdfa-editor-generic-model-plugin.js
@@ -12,9 +12,12 @@ import { task } from 'ember-concurrency';
 
 //order matters
 const INJECTCONTEXTS = [
+  { card: 'editor-plugins/resources-relation-card',
+    pattern: /.\/\S*:\S*/
+  },
   {
     card: 'editor-plugins/resources-card',
-    pattern: /[~\/|.\/]\S*:\S*/
+    pattern: /~\/\S*:\S*/
   },
   {
     card: 'editor-plugins/classes-card',
